0b320499767fe7caeee4accb6e3e6a686ab3839b
root <root@shaynet.shaydomain>
2020-05-06 12:09:14 +0200

    add foreach loop

--- index.php
+++ index.php
@@ -10,7 +10,16 @@
     // Using two forward slashes we create comments in php.
     // Within the php tag, we can insert values into our HTML.
     echo "<h1>Welcome to ".$name."!</h1>";
-    echo "<h2>You have $".$credit." in your wallet.</h2>";";
+    echo "<h2>You have $".$credit." in your wallet.</h2>";
+    
+        $products['Computer']=750;
+        $products['Car']=15000;
+        $products['iPhone']=1000;
+        $products['Toaster']=75;
+        
+        foreach($products as $key => $value){
+            echo "<p>The ".$key." costs ".$value."</p>";
+    }
     ?>
   </body>
 </html>
